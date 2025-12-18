67ff7a27eab66e40d3f176268dd3a7485f83ef26
norovsuren15 <e.norvoo@gmail.com>
2025-12-18 20:37:13 +0800

    v1

---
+++ script.js
@@ -0,0 +1,10 @@
+const peoploIntro = document.getElementById("intro");
+
+const box = () => {
+  const people = {
+    portrait: img,
+    name: "ner",
+    job: "mergejil",
+    intro: "introduction",
+  };
+};
