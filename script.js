7182a9e5cb8efbf93e1be30f5cd36bcc08fb4fc4
norovsuren15 <e.norvoo@gmail.com>
2025-12-19 12:36:20 +0800

    v1

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const peoploIntro = document.getElementById("intro");
 
-const box = () => {
-  const people = {
-    portrait: img,
+const peopleInfos = [
+  {
+    id: 1,
+    portrait: "img",
     name: "ner",
     job: "mergejil",
     intro: "introduction",
-  };
+  },
+  {
+    id: 2,
+    portrait: "img",
+    name: "ner",
+    job: "mergejil",
+    intro: "introduction",
+  },
+];
+
+console.log(peopleInfos);
+
+const selectContent = (peopleInfoId) => {
+  peoploIntro.innerHTML = peopleInfos.selectOnePeople;
 };
+
+console.log(selectContent());
