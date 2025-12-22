eca47a63bd04c84f69eee76e44eef4c319913599
norovsuren15 <e.norvoo@gmail.com>
2025-12-22 11:49:58 +0800

    js end

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 const peoploIntro = document.getElementById("intro");
+const surpriseBtn = document.getElementById("surprise-btn");
 
 const peopleInfos = [
   {
-    id: 1,
-    portrait: "img",
-    name: "ner",
-    job: "mergejil",
-    intro: "introduction",
+    portraitImg: "./img/zurag1.jpg",
+    name: "Susan Smith",
+    job: "WEB DEVELOPER",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
   {
-    id: 2,
-    portrait: "img",
-    name: "ner",
-    job: "mergejil",
-    intro: "introduction",
+    portraitImg: "./img/zurag2.jpg",
+    name: "Winter Watts",
+    job: "UX DESIGNER",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  },
+  {
+    portraitImg: "./img/zurag3.jpg",
+    name: "Jakob Lucero",
+    job: "BACKEND DEVELOPER",
+    intro: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
 ];
 
-console.log(peopleInfos);
+let index = 0;
 
-const selectContent = (peopleInfoId) => {
-  peoploIntro.innerHTML = peopleInfos.selectOnePeople;
+const currentPerson = peopleInfos[index];
+
+const next = () => {
+  index++;
+  selectContent();
 };
 
-console.log(selectContent());
+const pre = () => {
+  index--;
+  selectContent();
+};
+
+const selectContent = () => {
+  const person = peopleInfos[index];
+
+  if (index < peopleInfos.length) {
+    peoploIntro.innerHTML = ` 
+    <div class="person-container">
+      <img src="${person.portraitImg}" alt="" />
+      <h4>${person.name}</h4>
+      <h4>${person.job}</h4>
+      <p>${person.intro}</p>
+    </div>`;
+  }
+};
+
+selectContent(index);
